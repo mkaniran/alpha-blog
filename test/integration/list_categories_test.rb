6ccc43495ab6b19c4335e503e1c010f3e744b421
Mohammad Kazem Aniran <aniran.project@gmail.com>
2015-12-27 15:13:50 +0000

    Fix tests and update navbar with categories

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,15 +1,15 @@
 require 'test_helper'
 
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class ListCategoriesTest < ActionDispatch::IntegrationTest
    
   def setup
-    @category = Category.create(name: "books")
-    @category2 = Category.create(name = "programming")
+    @category = Category.create(name: "sports")
+    @category2 = Category.create(name: "programming")
   end
   
   test "should show categories listing" do
     get categories_path
-    assert_template 'category/index'
+    assert_template 'categories/index'
     assert_select "a[href=?]", category_path(@category), text: @category.name
     assert_select "a[href=?]", category_path(@category2), text: @category2.name
   end
